ae05647c5ea1111b277622bbf31d2c01b1f08f21
Scott Crawford <scottcrawford03@gmail.com>
2014-12-13 14:40:30 -0700

    initial commit

---
+++ main.rb
@@ -0,0 +1,56 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'slim'
+require 'sass'
+require './song'
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
+end
+
+not_found do
+  slim :not_found
+end
